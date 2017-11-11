be3ce1cb4389d9a40b279e11660887c17e190655
Canu667 <mcanu667@gmail.com>
2017-11-11 21:17:42 +0000

    [Progress] - JSX

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-const ce = React.createElement
-
-const MyTitle = function(props) {
-  return ce('dive', null, ce('h1', { style: { color: props.color } }, props.title))
+const MyTitle = props => {
+    const style = { color: props.color };
+    return (
+        <div>
+          <h1 style={style}>
+              {props.title}
+          </h1>
+        </div>
+    );
 };
 
-const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'peru' })
+const MyFirstComponent = () => {
+  return (
+    <div>
+        <MyTitle title= 'Game of Thrones' color='YellowGreen' />
+        <MyTitle title= 'Stranger Things' color='GreenYellow' />
+        <MyTitle title= 'Rick and Morty' color='LimeGreen' />
+        <MyTitle title= 'House of Cards' color='peru' />
+
+    </div>
   );
 };
 
-render(ce(MyFirstComponent), document.getElementById('app'))
+render(<MyFirstComponent />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 const path = require('path')
 
 module.exports = {
-    context:__dirname, //running this from root directory - ALWAYS
-    entry: './js/ClientApp.jsx', //front door to your application. Everything is included from here
-    devtool: 'cheap-eval-source-map', //inline all my source maps into my code
+    context:__dirname, /* running this from root directory - ALWAYS */
+    entry: './js/ClientApp.jsx', /* front door to your application. Everything is included from here */
+    devtool: 'cheap-eval-source-map', /* inline all my source maps into my code */
     output: {
-        path: path.join(__dirname, 'public'), //we will alwyas land in public directory
+        path: path.join(__dirname, 'public'), /* we will always land in public directory */
         filename: 'bundle.js'
     },
     resolve: {
         extensions: ['.js', '.jsx', '.json']
     },
     stats: {
-        colors: true, //color in terminal
+        colors: true, /* color in terminal */
         reasons: true,
         chunks: true
     },
-    module: { //it uses some kind of rule on your code
+    module: { /* it uses some kind of rule on your code */
         rules: [
             {
                 test: /\.jsx?$/,
