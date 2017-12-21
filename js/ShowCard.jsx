8e6b0fc1efb9d3faa0ee0f229c73a24600f9d48a
Canu667 <mcanu667@gmail.com>
2017-12-21 06:27:25 +0000

    [Progress] - link from ShowCard + fixing

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -2,14 +2,17 @@
 
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const Wrapper = styled.div`
+const Wrapper = styled(Link)`
   width: 32%;
   border: 2px solid #333;
   border-radius: 4px;
   margin-bottom: 25px;
   padding-right: 10px;
   overflow: hidden;
+  text-decoration: none;
+color: black;
 `;
 
 const Image = styled.img`
@@ -19,21 +22,16 @@ const Image = styled.img`
 `;
 
 const ShowCard = (
-    props: {
-        poster: string,
-        title: string,
-        year: string,
-        description: string
-    }
+    props: Show
 ) => (
-    <Wrapper>
-        <Image alt={`${props.title} Show Poster`} src={`/public/img/posters/${props.poster}`} />
-        <div>
-            <h3>{props.title}</h3>
-            <h4>({props.year})</h4>
-            <p>{props.description}</p>
-        </div>
-    </Wrapper>
+        <Wrapper to={`/details/${props.imdbID}`}>
+            <Image alt={`${props.title} Show Poster`} src={`/public/img/posters/${props.poster}`} />
+            <div>
+                <h3>{props.title}</h3>
+                <h4>({props.year})</h4>
+                <p>{props.description}</p>
+            </div>
+        </Wrapper>
 );
 
 export default ShowCard;
